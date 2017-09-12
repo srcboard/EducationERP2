97fe926788fe326109a1dad13ce55f59d2f1d8b0
user <user@user.com>
2017-09-12 09:59:51 +0300

    Initialization

---
+++ src/main/java/com/example/EducationErp2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationErp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationErp2Application.class, args);
+	}
+}
